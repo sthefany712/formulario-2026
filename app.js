ab02a33f26bf9415c49155502b963d0e747ae5f8
Sthefany Correia <sthefanycorreia712@gmail.com>
2026-05-09 17:37:49 -0300

    Ajustes no código

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 'use strict'
 
 //Não tinhamos isso nas outras atividades, antes era um json
-async function getDados(cep){ //funções aync são mais lentas por ter que esperar resposta
+async function getDadosCep(cep){ //funções aync são mais lentas por ter que esperar resposta
     const url = `https://viacep.com.br/ws/${cep}/json/`
     const response = await fetch(url)
     const data = await response.json()
     return data
 
-    //console.log(data);
+    //console.log(data);+
 }
 
 async function preencherFormulario (){
     // console.log('teste')
     const cep = document.getElementById('cep').value 
-    const dadosCep = await getDados(cep) //Vai ir no back e trazer as informações
-    
+    const dadosCep = await getDadosCep(cep) //Vai ir no back e trazer as informações
     document.getElementById('endereco').value = dadosCep.logradouro
     document.getElementById('bairro').value = dadosCep.bairro
     document.getElementById('cidade').value = dadosCep.localidade
@@ -30,10 +29,10 @@ async function preencherFormulario (){
 
 document.getElementById('cep')
     .addEventListener('focusout', preencherFormulario) //2 parametros, 1 ação q quer capturar e a 2 oq fazer //
-    //change, o sistema verificar cada digito, se coloco 1 ele vai na API e ve se tem...*
+    //change, o sistema vai verificar cada digito, se coloco 1 ele vai na API e ve se tem...*
     //quem estarta preencherFormulario é o addEventListener (função CallBack)
 
-
+//O evento focusout no JavaScript é disparado quando um elemento, ou qualquer elemento dentro dele, perde o foco (o usuário clica fora ou usa Tab para sair).
 
 
 
